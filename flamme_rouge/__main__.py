12a5472fa55f1e75f33cf65da3cffc6878a30690
Markus Shepherd <markus@idtmessaging.com>
2018-08-15 16:54:47 +0300

    first rough version of __main__

---
+++ flamme_rouge/__main__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+
+from .core import FRGame, Rouleur, Section, Sprinteur, Strategy, Team, Track
+
+LOGGER = logging.getLogger(__name__)
+
+def _main():
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)-8.8s [%(name)s:%(lineno)s] %(message)s'
+    )
+
+    length = 9 * 6 + 12 * 2
+    sections = map(Section, range(length))
+    track = Track(sections)
+
+    names = ('red', 'blue', 'green', 'black')
+    teams = []
+    strategy = Strategy()
+
+    for name in names:
+        sprinteur = Sprinteur()
+        rouleur = Rouleur()
+        team = Team(name=name, cyclists=(sprinteur, rouleur), strategy=strategy)
+        sprinteur.team = team
+        rouleur.team = team
+        teams.append(team)
+
+        LOGGER.info('Team <%s> with members <%s> and <%s>', team, sprinteur, rouleur)
+
+    game = FRGame(track, teams)
+    game.play()
+
+    LOGGER.info('winner: %s', game.track.leading())
+
+
+if __name__ == '__main__':
+    _main()
--- flamme_rouge/core.py
+++ flamme_rouge/core.py
@@ -157,6 +157,15 @@ class Track:
                 for cyclist in sec0.cyclists:
                     cyclist.discard(EXHAUSTION_CARD)
 
+    def leading(self):
+        ''' leading cyclist '''
+
+        for section in reversed(self.sections):
+            if not section.empty():
+                return section.cyclists[0]
+
+        return None
+
     def finished(self):
         ''' game finished '''
 
@@ -221,7 +230,7 @@ class Cyclist:
     def __str__(self):
         if self.team is None:
             return self.__class__.__name__
-        return '{:s} ({:s})'.format(self.__class__.__name__, self.team)
+        return '{:s} ({:s})'.format(self.__class__.__name__, str(self.team))
 
 
 class Rouleur(Cyclist):
@@ -243,8 +252,8 @@ class Team:
 
     def __init__(self, name, cyclists, strategy=None):
         self.name = name
-        self.cyclists = cyclists
-        self.strategy = strategy
+        self.cyclists = tuple(cyclists)
+        self.strategy = strategy if strategy is not None else Strategy()
 
     def __str__(self):
         return self.name
@@ -253,11 +262,27 @@ class Team:
 class Strategy:
     ''' strategy '''
 
+    def starting_positions(self, team, game):
+        ''' select starting positions '''
+
+        result = {}
+
+        available = [
+            section for section in game.track.sections[:game.track.start] if not section.full()]
+
+        for cyclist in team.cyclists:
+            section = choice(available)
+            result[cyclist] = section
+            if len(section.cyclists) + 1 >= section.lanes:
+                available.remove(section)
+
+        return result
+
     #pylint: disable=no-self-use,unused-argument
     def next_cyclist(self, team, game=None):
         ''' select the next cyclist '''
 
-        available = [cyclist for cyclist in team.cyclist if cyclist.curr_card is None]
+        available = [cyclist for cyclist in team.cyclists if cyclist.curr_card is None]
         return choice(available) if available else None
 
     def cyclists(self, team, game=None):
@@ -281,7 +306,20 @@ class FRGame:
 
     def __init__(self, track, teams):
         self.track = track
-        self.teams = tuple(teams)
+        self.teams = list(teams)
+        shuffle(self.teams)
+        self.teams = tuple(self.teams)
+        self.rounds_played = 0
+
+    def starting_positions(self):
+        ''' initiate the game '''
+
+        for team in self.teams:
+            for cyclist, section in team.strategy.starting_positions(team, self).items():
+                section.add_cyclist(cyclist)
+
+        LOGGER.info('starting positions:')
+        LOGGER.info(self)
 
     def play_round(self):
         ''' play a round '''
@@ -289,19 +327,27 @@ class FRGame:
         for team in self.teams:
             for cyclist in team.strategy.cyclists(team, self):
                 cyclist.draw_hand()
+                # hand = tuple(cyclist.hand)
                 card = team.strategy.choose_card(cyclist, team, self)
                 cyclist.select_card(card)
                 cyclist.discard_hand()
 
         for cyclist in self.track.cyclists():
-            self.track.move_cylist(cyclist, cyclist.curr_card)
+            self.track.move_cyclist(cyclist, cyclist.curr_card)
             cyclist.curr_card = None
 
         self.track.do_slipstream()
         self.track.do_exhaustion()
 
+        self.rounds_played += 1
+
+        LOGGER.info('after %d rounds:', self.rounds_played)
+        LOGGER.info(self)
+
     def play(self):
         ''' play the game '''
+
+        self.starting_positions()
 
         while not self.track.finished():
             self.play_round()
